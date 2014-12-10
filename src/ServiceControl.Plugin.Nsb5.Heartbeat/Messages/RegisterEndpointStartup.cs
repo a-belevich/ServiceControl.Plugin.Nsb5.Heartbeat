8d044d842d27fbd6d2f862f76f39f692503d3e2a
Greg Bielleman <greg.bielleman@particular.net>
2014-12-10 14:38:42 +1100

    Cleanup project structure

--- src/ServiceControl.Plugin.Nsb5.Heartbeat/Messages/RegisterEndpointStartup.cs
+++ src/ServiceControl.Plugin.Nsb5.Heartbeat/Messages/RegisterEndpointStartup.cs
@@ -1,18 +1,7 @@
-﻿namespace ServiceControl.Plugin.Heartbeat.Messages
+namespace ServiceControl.Plugin.Heartbeat.Messages
 {
     using System;
     using System.Collections.Generic;
-
-    class EndpointHeartbeat
-    {
-        public DateTime ExecutedAt { get; set; }
-
-        public string EndpointName { get; set; }
-
-        public Guid HostId { get; set; }
-
-        public string Host { get; set; }
-    }
 
     class RegisterEndpointStartup
     {
